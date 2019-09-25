520419abb244a23b78c9ecb3424cfd5604554418
mengzel <shiloving@163.com>
2019-09-25 19:27:56 +0800

    初始化项目完成

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: true,
+  // 实时保存、编译的配置段
+  devServer: {
+    // 自动开启浏览器
+    open: true,
+    // 服务器运行的端口
+    port: 12309,
+    // 运行服务的主机名（电脑的真实ip，不能乱定义）
+    host: '127.0.0.1'
+  }
+
 }
